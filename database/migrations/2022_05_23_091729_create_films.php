e3a420cb67efb7e3cb556abc12dbeefc94360b22
Flo Tlm <tailame.florent@gmail.com>
2022-05-23 14:09:07 +0400

    mise en place de la bdd

---
+++ database/migrations/2022_05_23_091729_create_films.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->string('titre',150);
+            $table->string('resume',2000);
+            $table->string('duree',150);
+            $table->string('affiche',1024);
+            $table->string('casting',200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+};
