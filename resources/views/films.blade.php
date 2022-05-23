d5d73ccbfa3eb7a324444f6687ca83c1f70a2b44
Flo Tlm <tailame.florent@gmail.com>
2022-05-23 15:47:06 +0400

    Ajout des routes,des Layouts  et teste de la page films

---
+++ resources/views/films.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+
+
+@section('main')
+    <table class="rounded-t-lg m-5 w-5/6 mx-auto  bg-gray-700 text-gray-200">
+        <tr class="text-left border-b border-gray-300">
+            <th class="px-4 py-3">Titres</th>
+      
+
+        </tr>
+       
+            <tr class="bg-gray-700 border-b border-gray-600">
+                <td class="px-4 py-3">Films</td>
+                
+            </tr>
+        
+    </table>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <title>Films</title>
+
+    <header>
+        @include('layouts.header')
+    </header>
+
+</head>
+
+<body>
+
+
+    @yield('header')
+
+    <main>
+        @yield('main')
+    </main>
+
+    <main>
+        @yield('contenu')
+    </main>
+
+  
+
+    <!-- footer -->
+    <footer>
+        @include('layouts.footer')
+    </footer>
+
+
+
+</body>
+
+</html>
